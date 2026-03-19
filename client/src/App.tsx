c98ad424697e6f0ec29ffc2a6528cf05e31e87fb
Lloyd dela Cruz <lloyd.delacruz@outlook.com>
2026-03-18 21:56:37 -0700

    fix some issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AuthProvider } from "@/contexts/AuthContext";
 import MainLayout from "@/layouts/MainLayout";
 import Home from "@/pages/Home";
 import About from "@/pages/About";
@@ -36,12 +35,10 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
